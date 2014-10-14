3848d1257302024afc0d6d15b7c9824c2a87f08a
Vladimir Kuznetsov <vavaka@gmail.com>
2014-10-14 14:35:47 +0600

    Initial Commit

---
+++ YellTestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YellTestApp
+//
+//  Created by Vladimir Kuznetsov on 10/14/14.
+//  Copyright (c) 2014 Yell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
